e0984043ed01d4fb9026a34cda100fa33e7ad001
Joy Xi <joy.b.xi@gmail.com>
2016-03-10 12:51:26 -0800

    Added comments. Still trying to fix bug

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,39 +15,45 @@ class ViewController: UIViewController {
     
     var userIsInTheMiddleOfTypingANumber = false
     var userHasTypedDecimalPoint = false
+    
+    var operandStack = Array<Double>()
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         // print("digit = \(digit)")
         if userIsInTheMiddleOfTypingANumber {
+            // if user is typing something, add that digit to the number
             display.text = display.text! + digit
         } else {
+            // if user isn't in middle of typing something, reset screen to the digit just pressed and say we're typing now
             display.text = digit
             userIsInTheMiddleOfTypingANumber = true
         }
     }
     
-    var operandStack = Array<Double>()
-    
     @IBAction func modifyNumber(sender: UIButton) {
         let modifier = sender.currentTitle!
         switch modifier {
         case ".":
-            // only respond to "." key if user haven't typed a decimal point yet and if user is in middle of typing a number
-            if (!userHasTypedDecimalPoint && userIsInTheMiddleOfTypingANumber) {
-                // append a decimal point and add it to display test, and indicate that user has typed a decimal point and has started typing
-                display.text = display.text! + modifier
+            // only respond to "." key if user haven't typed a decimal point yet
+            if (!userHasTypedDecimalPoint) {
+                // if user is in middle of typing a number...
+                if (userIsInTheMiddleOfTypingANumber) {
+                    // append a decimal point and add it to display test, and indicate that user has typed a decimal point and has started typing
+                    display.text = display.text! + modifier
+                } else {
+                    // if user is not in the middle of typing a number, should add 0.
+                    display.text = "0."
+                    userIsInTheMiddleOfTypingANumber = true
+                }
                 userHasTypedDecimalPoint = true
-            } else if (!userIsInTheMiddleOfTypingANumber) {
-                // if user is not in the middle of typing a number, should add 0.
-                display.text = "0."
-                userIsInTheMiddleOfTypingANumber = true
             }
         default: break
         }
     }
     
     @IBAction func enter() {
+        // stop typing number, clear decimal point tracker, add current value to the stack
         userIsInTheMiddleOfTypingANumber = false
         userHasTypedDecimalPoint = false
         operandStack.append(displayValue)
@@ -56,9 +62,11 @@ class ViewController: UIViewController {
     
     @IBAction func operate(sender: UIButton) {
         let operation = sender.currentTitle!
+        // pressing operate button should implicitly enter whatever user has on screen if we're in middle of typing
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
+        // whichever operation we want, perform it by popping numbers off the stack
         switch operation {
         case "✕": performOperation { $0 * $1 }
         case "÷": performOperation { $1 / $0 }
@@ -70,6 +78,7 @@ class ViewController: UIViewController {
     }
     
     private func performOperation(operation: (Double, Double) -> Double) {
+        // if there are enough operands in the stack, change the displayValue to the result of the operation, and save that value onto the stack
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
             enter()
@@ -83,12 +92,12 @@ class ViewController: UIViewController {
         }
     }
     
-    
     var displayValue: Double {
         get {
             return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
         }
         set {
+            // set the display value and stop typing
             display.text = "\(newValue)"
             userIsInTheMiddleOfTypingANumber = false
         }
